3eb4224971cd5319b477e4426417608055919379
ga-devfront <root@GameComputer.localdomain>
2020-02-29 16:41:29 +0100

    ➕ features section

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,19 @@
 <template>
     <main class="container center column spaceBottom">
       <SearchBar />
+      <Features />
     </main>
 </template>
 
 <script>
 import SearchBar from './SearchBar.vue';
+import Features from './Features.vue';
 
 export default {
   name: 'myMain',
   components: {
     SearchBar,
+    Features,
   },
 };
 </script>
@@ -18,7 +21,6 @@ export default {
 <style scoped>
 main {
     position: relative;
-    bottom: -40px;
     min-height: 100%;
     -webkit-transition: ease 1s;
     transition: ease 1s;
