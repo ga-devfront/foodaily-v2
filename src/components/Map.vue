416d73d1a215be1f31508e031e9aadc4f4221332
ga-devfront <root@GameComputer.localdomain>
2020-03-05 18:54:07 +0100

    ✨ add results with google places

---
+++ src/components/Map.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="container center column">
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/step/Step1.vue
+++ src/components/step/Step1.vue
@@ -3,13 +3,25 @@
       <aside id="resultName" class="container verticalCenter column center spaceBottom">
         <p class="big white bold">Restaurants à {{research.locality}}</p>
       </aside>
+      <article class="container center column">
+        <nav class="container between spaceBottom">
+          <a class="bold white button"><< Retour</a>
+          <p id="resultNumber" class="bold"></p>
+        </nav>
+        <section>
+          <Map />
+        </section>
+      </article>
     </section>
 </template>
 
 <script>
+import Map from '../Map.vue';
+
 export default {
-  name: 'Step0',
+  name: 'Step1',
   components: {
+    Map,
   },
   props: {
     research: {
@@ -19,9 +31,30 @@ export default {
   },
   data() {
     return {
+      city: this.research,
+      results: [],
     };
   },
   methods: {
+  },
+  created() {
+    /* eslint-disable-next-line */
+    const pos = new google.maps.LatLng(this.city.latitude,this.city.longitude);
+    const request = {
+      location: pos,
+      radius: '500',
+      type: ['restaurant'],
+    };
+    /* eslint-disable-next-line */
+    const service = new google.maps.places.PlacesService(document.createElement('div'));
+    service.nearbySearch(request, (results, status) => {
+      /* eslint-disable-next-line */
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        results.forEach((result) => {
+          this.results.push(result);
+        });
+      }
+    });
   },
 };
 </script>
@@ -31,5 +64,26 @@ export default {
     width: 100%;
     background: url("../../assets/resultBackground.jpg");
     height: 150px;
+}
+
+.button {
+    background-color: #0067c0;
+    font-variant: small-caps;
+    font-size: 1.1em;
+    border-radius: 0.5em;
+    padding: 10px 20px 10px 20px;
+    -webkit-transition: ease 0.5s;
+    transition: ease 0.5s;
+}
+
+.button:hover {
+    background-color: #007eea;
+    cursor: pointer;
+}
+
+#resultNumber {
+    line-height: 80%;
+    color: #222222;
+    font-style: italic;
 }
 </style>
