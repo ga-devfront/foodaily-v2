c24f9a365ecdcc6ca25be63656e943e50ac2d16f
ga-devfront <root@GameComputer.localdomain>
2020-03-17 16:25:50 +0100

    📦 add Vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './App.vue';
-import store from './store';
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuex);
+const store = new Vuex.Store({
+  state: {
+    restaurants: {
+      summary: {},
+      details: {},
+    },
+  },
+  mutations: {
+    addRestaurant(type, data) {
+      this.restaurants[type][data.id] = data;
+    },
+  },
+});
 new Vue({
   store,
   render: (h) => h(App),
